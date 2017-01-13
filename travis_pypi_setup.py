57f8eaefe214441e778569a62519e7ce0a513aa6
Zenohm <sentherus@gmail.com>
2017-01-12 19:08:49 -0500

    Travis PyPI setup run Autogenerated PyPI setup

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'Zenohm/friday'
+GITHUB_REPO = 'Zenohm/Friday'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
